f51fb6f227cb0e3e25b4e8c3e519a5ea68ea7b43
Miguel Muñiz <73082309+MiguelMunizJR@users.noreply.github.com>
2023-04-25 20:55:09 -0600

    add eslint config and typescript

--- src/App.jsx
+++ src/App.jsx
@@ -12,113 +12,113 @@ import { useEffect, useState } from "react";
 import Loading from "./components/routes/Loading";
 
 function App() {
-  const [cartProducts, setCartProducts] = useState();
-  const [isEmpty, setIsEmpty] = useState(true);
-  const [total, setTotal] = useState(0);
-  const [isLogin, setIsLogin] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+	const [cartProducts, setCartProducts] = useState();
+	const [isEmpty, setIsEmpty] = useState(true);
+	const [total, setTotal] = useState(0);
+	const [isLogin, setIsLogin] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
 
-  const getAllProductsCart = () => {
-    const URL = "https://ecommerce-api-react.herokuapp.com/api/v1/cart";
-    axios
-      .get(URL, getConfig())
-      .then((res) => {
-        const products = res.data.data.cart.products;
-        setCartProducts(products);
-        const total = products?.reduce((acc, cv) => {
-          return Number(cv.price) * cv.productsInCart.quantity + acc;
-        }, 0);
-        setTotal(total);
-        setIsEmpty(false);
-      })
-      .catch();
-  };
+	const getAllProductsCart = () => {
+		const URL = "https://ecommerce-api-react.herokuapp.com/api/v1/cart";
+		axios
+			.get(URL, getConfig())
+			.then((res) => {
+				const products = res.data.data.cart.products;
+				setCartProducts(products);
+				const total = products?.reduce((acc, cv) => {
+					return Number(cv.price) * cv.productsInCart.quantity + acc;
+				}, 0);
+				setTotal(total);
+				setIsEmpty(false);
+			})
+			.catch();
+	};
 
-  useEffect(() => {
-    const token = localStorage.getItem("token");
+	useEffect(() => {
+		const token = localStorage.getItem("token");
 
-    if (token) {
-      getAllProductsCart();
-      setIsLogin(true);
-    }
+		if (token) {
+			getAllProductsCart();
+			setIsLogin(true);
+		}
 
-    // Crear usuario
-    // const URL = 'https://ecommerce-api-react.herokuapp.com/api/v1/users';
-    // const Obj = {
-    //   firstName: "Alpha",
-    //   lastName: "User",
-    //   email: "alpha_user100@gmail.com",
-    //   password: "123456",
-    //   phone: "3322991188",
-    //   role: "admin",
-    // };
+		// Crear usuario
+		// const URL = 'https://ecommerce-api-react.herokuapp.com/api/v1/users';
+		// const Obj = {
+		//   firstName: "Alpha",
+		//   lastName: "User",
+		//   email: "alpha_user100@gmail.com",
+		//   password: "123456",
+		//   phone: "3322991188",
+		//   role: "admin",
+		// };
 
-    // axios.post(URL, Obj)
-    // .then((res) => console.log(res.data))
-    // .catch((err) => console.log(err));
-  }, [isLogin]);
+		// axios.post(URL, Obj)
+		// .then((res) => console.log(res.data))
+		// .catch((err) => console.log(err));
+	}, [isLogin]);
 
-  return (
-    <div className="App">
-      <Loading isLoading={isLoading} />
-      <Header
-        getAllProductsCart={getAllProductsCart}
-        cartProducts={cartProducts}
-        total={total}
-        setTotal={setTotal}
-        isEmpty={isEmpty}
-        setIsEmpty={setIsEmpty}
-        setIsLoading={setIsLoading}
-        isLogin={isLogin}
-      />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Home
-              getAllProductsCart={getAllProductsCart}
-              cartProducts={cartProducts}
-              isEmpty={isEmpty}
-              setIsEmpty={setIsEmpty}
-              setIsLoading={setIsLoading}
-            />
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <Login
-              setIsLoading={setIsLoading}
-              isLogin={isLogin}
-              setIsLogin={setIsLogin}
-              setIsEmpty={setIsEmpty}
-              setTotal={setTotal}
-            />
-          }
-        />
-        <Route
-          path="/products/:id"
-          element={
-            <ProductDetails
-              getAllProductsCart={getAllProductsCart}
-              cartProducts={cartProducts}
-              isLogin={isLogin}
-              isEmpty={isEmpty}
-              setIsEmpty={setIsEmpty}
-              setIsLoading={setIsLoading}
-            />
-          }
-        />
-        <Route element={<ProtectedRoutes />}>
-          <Route
-            path="/purchases"
-            element={<Purchases setIsLoading={setIsLoading} />}
-          />
-        </Route>
-      </Routes>
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Loading isLoading={isLoading} />
+			<Header
+				getAllProductsCart={getAllProductsCart}
+				cartProducts={cartProducts}
+				total={total}
+				setTotal={setTotal}
+				isEmpty={isEmpty}
+				setIsEmpty={setIsEmpty}
+				setIsLoading={setIsLoading}
+				isLogin={isLogin}
+			/>
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<Home
+							getAllProductsCart={getAllProductsCart}
+							cartProducts={cartProducts}
+							isEmpty={isEmpty}
+							setIsEmpty={setIsEmpty}
+							setIsLoading={setIsLoading}
+						/>
+					}
+				/>
+				<Route
+					path="/login"
+					element={
+						<Login
+							setIsLoading={setIsLoading}
+							isLogin={isLogin}
+							setIsLogin={setIsLogin}
+							setIsEmpty={setIsEmpty}
+							setTotal={setTotal}
+						/>
+					}
+				/>
+				<Route
+					path="/products/:id"
+					element={
+						<ProductDetails
+							getAllProductsCart={getAllProductsCart}
+							cartProducts={cartProducts}
+							isLogin={isLogin}
+							isEmpty={isEmpty}
+							setIsEmpty={setIsEmpty}
+							setIsLoading={setIsLoading}
+						/>
+					}
+				/>
+				<Route element={<ProtectedRoutes />}>
+					<Route
+						path="/purchases"
+						element={<Purchases setIsLoading={setIsLoading} />}
+					/>
+				</Route>
+			</Routes>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
