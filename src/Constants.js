ac2e63259650d7c1642fb4d797651a38e193358f
Miguel Muñiz <73082309+MiguelMunizJR@users.noreply.github.com>
2023-05-11 23:57:31 -0600

    add constants file

---
+++ src/Constants.js
@@ -0,0 +1,8 @@
+export const ROUTES_PATH = Object.freeze({
+	HOME: "/",
+	LOGIN: "/auth/login",
+	REGISTER: "/auth/register",
+	PRODUCT: "/products/:id",
+	ORDERS: "/orders",
+	USER: "/users/me"
+});
