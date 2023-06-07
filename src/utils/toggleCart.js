bc55211b8dbcb0928a2a00222a521c8ca9ac5277
Miguel Muñiz <73082309+MiguelMunizJR@users.noreply.github.com>
2023-06-07 10:22:43 -0600

    add validate token on toggle cart

--- src/utils/toggleCart.js
+++ src/utils/toggleCart.js
@@ -1,8 +1,14 @@
-const toggleCart = (isLogin, getAllProductsCart) => {
+import { checkTokenValidity } from "./auth/authServices";
+
+const toggleCart = (isLogin, getAllProductsCart, storedToken) => {
 	const cartSlider = document.querySelector(".cart");
 	cartSlider.classList.toggle("cart__active");
 	const isOpen = Boolean(cartSlider.classList.contains("cart__active"));
-	
+
+	if (isLogin) {
+		checkTokenValidity(storedToken);
+	}
+
 	const getProductsCart = isOpen && isLogin;
 	getProductsCart && getAllProductsCart();
 };
